998d440c50d4cf80efc4edaafd7c335517a67175
Fabricio <earthbornshepard@hotmail.com>
2020-07-13 10:44:06 -0300

    Primeiro commit.

---
+++ src/components/SignUpPage/index.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const SignUpPage = () => {
+  return <div>SignUpPage</div>;
+};
+
+export default SignUpPage;
---
+++ src/components/UserProfilePage/index.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+
+const UserProfilePage = () => {
+  return <div>UserProfilePage</div>;
+};
+
+export default UserProfilePage;

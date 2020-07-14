db6dfa3edcec3bfb7b0677d92cf297432e994af3
Lais <laispetrav@gmail.com>
2020-07-14 12:18:19 -0300

    style LoginPage

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
-import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
-//import Logo from "../../logo-future-eats@2x.png";
 
-const PageConteiner = styled.div `
-display: flex;
-flex-direction: column;
-align-items: center;
-`
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
-//xport const ImgBox = styled.div`
-//align-items: center;
-//justify-items: center;
-//`;
+
+import {
+  PageConteiner,
+  Logo,
+  Tittle,
+  InputContainer,
+  Input,
+  Button
+} from './styles'
+
+
+import LogoImage from './logo.svg';
+
 
 
 const BaseUrl = "https://us-central1-missao-newton.cloudfunctions.net/rappi4b/login"
@@ -46,31 +50,49 @@ const LoginPage = () => {
   };
 
 
+
   return (
-  <PageConteiner>
-    <h1>LOGIN PAGE</h1>
-    <div>Entrar</div>
-    <form>
-      <label htmlFor="email">E-mail</label>
-      <input
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        id="email"
-        type="email"
-        required
-      />
-      <label htmlFor="password">Senha</label>
-      <input
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        id="password"
-        type="password"
-        required
-     />
-      <button onClick={handleLogin}>Entrar</button>
-    </form>
-    <button onClick={goToSignUpPage}>Cadastre-se!</button>
-  </PageConteiner>
+    <PageConteiner>
+      <Logo src={LogoImage} />
+      <Tittle>Entrar</Tittle>
+      <InputContainer>
+        <TextField
+          label="E-mail"
+          style={{ margin: 8 }}
+          placeholder="email@email.com"
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+          type="email"
+          required
+          fullWidth
+        />
+        <TextField
+          label="Senha"
+          style={{ margin: 8 }}
+          placeholder="Minimo 6 caracteres"
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="outlined"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id="password"
+          type="password"
+          required
+          fullWidth
+        />
+
+        <Button onClick={handleLogin}><b>Entrar</b></Button>
+      </InputContainer>
+      <p onClick={goToSignUpPage}>Não tem cadastro? Clique aqui</p>
+    </PageConteiner>
   )
 
 };
--- src/components/LoginPage/styles.js
+++ src/components/LoginPage/styles.js
@@ -1,96 +1,35 @@
 import styled from 'styled-components'
 
-export const Feed = styled.div`
-    width: 360px;
-    height: 640px;
-    font-family: Roboto;
-`
-export const Header = styled.div`
-    width: 360px;
-    height: 64px;
-    -webkit-backdrop-filter: blur(10px);
-    backdrop-filter: blur(10px);
-    box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);
-    display: flex;
-    justify-content: space-around;
-`
-export const Tittle = styled.p`
-    margin-top: 43px;
-    letter-spacing: -0.39px;
-    text-align: center;
-    color: var(--black);
-`
-export const InputContainer = styled.div`
-    margin-top: 8px;
-    display: flex; 
-    justify-content: space-around;
-`
-export const Input = styled.input`
-    width: 328px;
-    height: 56px;
-    border-radius: 2px;
-    border: solid 1px #b8b8b8;
-    font-family: Roboto;
-    font-size: 16px;
-`
-export const FilterContainer = styled.div`
-    margin-top: 8px;
-    margin-bottom: 8px;
-    width: 360px;
-    height: 42px;
-    display: flex;
-    overflow: auto;
-    white-space: nowrap;
-`
-export const FilterKey = styled.p`
-    margin-left: 24px;
-    :active {
-      color: #e86e5a;
-    }
-`
-export const RestaurantContainer = styled.div`
+export const PageConteiner = styled.div `
     display: flex;
     flex-direction: column;
     align-items: center;
+    font-family: Roboto;
 `
-export const RestCard = styled.div`
-    width: 328px;
-    height: 200px;
-    border-radius: 8px;
-    border: solid 1px #b8b8b8;
+export const Logo = styled.img`
+    width: 104px;
+    height: 58px;
+    margin-top: 88px;
+    margin-bottom: 28px;
+`
+export const Tittle = styled.div`
+    
     margin-bottom: 8px;
 `
-export const RestLogo = styled.img`
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     width: 328px;
-    height: 120px;
-    border-top-left-radius: 8px;
-    border-top-right-radius: 8px;
 `
-export const CardName = styled.p`
-    color: #e86e5a;
-    margin: 4px 16px 0px 16px;
-`
-export const CardFooter = styled.div`
-    display: flex;
-    justify-content: space-between;
-    margin-right: 16px;
-    margin-left: 16px;
-`
-export const CardInfo = styled.p`
+export const Button = styled.button`
+    width: 328px;
+    height: 42px;
+    background-color: #e86e5a;
+    border-radius: 2px;
+    font-family: Roboto;
     font-size: 16px;
-    letter-spacing: -0.39px;
-    color: #b8b8b8;
-` 
-export const Footer = styled.div`
-    position: fixed;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    height: 49px;
-    box-shadow: 0 -1px 3px 0 rgba(0, 0, 0, 0.2), 
-      0 -2px 1px -1px rgba(0, 0, 0, 0.12), 
-      0 -1px 1px 0 rgba(0, 0, 0, 0.14);
-    background-color: white;
-    display: flex;
-    justify-content: space-around;
+    margin: 16px;
+    border: none;
+    
 `

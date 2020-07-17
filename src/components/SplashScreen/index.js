c43c0239d270a320961290fad9f559cb59c57f27
atmirala <atmirala@gmail.com>
2020-07-17 19:36:28 -0300

    Cor background splash screen

--- src/components/SplashScreen/index.js
+++ src/components/SplashScreen/index.js
@@ -5,7 +5,7 @@ import LogoImage from "./logoSplash.svg";
 const Div = styled.div`
 width: 100vw;
 height: 100vh;
-background-color: orange;
+background-color: #e86e5a;
 color: white;
 animation-name: fadeOutOpacity;
 animation-iteration-count: 1;
